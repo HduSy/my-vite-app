8ac81f74c73fc85bf815dff4ba18a41ae8dcc402
HduSy <hdusy14032122@gmail.com>
2024-01-08 14:44:59 +0800

    feat: Lottie

---
+++ src/components/LottieDemo.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import lottieWeb from 'lottie-web';
+import { onMounted, ref } from 'vue';
+import loading from '../assets/loading/loading.json';
+
+const el = ref(null)
+onMounted(() => {
+  lottieWeb.loadAnimation({
+    container: el.value,
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    animationData: loading
+  })
+})
+</script>
+
+<template>
+  <div id="container" ref="el"></div>
+</template>
+
+
+<style scoped lang="scss">
+#container {
+  width: 200px;
+  height: 200px;
+  background-color: #06aeec;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ const SpinePlayerAnime = () => import('../components/SpinePlayerAnime.vue')
 const HelloWorld = () => import('../components/HelloWorld.vue')
 const SVGAAnime = ()=>import('../components/SVGAAnime.vue')
 const CSSModule = () => import('../components/CSSModule.vue')
+const LottieAnime  = () => import('../components/LottieDemo.vue')
 
 const routes = [
   {path: '/', component: HelloWorld},
@@ -13,8 +14,8 @@ const routes = [
   {path: '/pixi-spine-demo', component: PixiSpineAnime},
   {path: '/spine-player-demo', component: SpinePlayerAnime},
   {path: '/svgademo', component: SVGAAnime},
-  {path: '/css-module', component: CSSModule}
-
+  {path: '/css-module', component: CSSModule},
+  {path: '/lottie-demo', component: LottieAnime}
 ]
 
 const router = createRouter({
