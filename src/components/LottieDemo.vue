0f805d33166bd10382337be5e251cea8869faf31
HduSy <hdusy14032122@gmail.com>
2024-01-08 15:20:02 +0800

    feat: lottie play control button

--- src/components/LottieDemo.vue
+++ src/components/LottieDemo.vue
@@ -1,15 +1,26 @@
 <script lang="ts" setup>
-import lottieWeb from 'lottie-web';
+import lottieWeb, { LottiePlayer } from 'lottie-web';
 import { onMounted, ref } from 'vue';
 import loading from '../assets/loading/loading.json';
 
 const el = ref(null)
+let animation: LottiePlayer
+const played = ref(false)
+const playorstopLottieAnime = () => {
+  if(animation && !played.value) {
+    animation.play()
+    played.value = true
+  } else {
+    animation.pause()
+    played.value = false
+  }
+}
 onMounted(() => {
-  lottieWeb.loadAnimation({
+  animation = lottieWeb.loadAnimation({
     container: el.value,
     renderer: 'svg',
     loop: true,
-    autoplay: true,
+    autoplay: false,
     animationData: loading
   })
 })
@@ -17,6 +28,7 @@ onMounted(() => {
 
 <template>
   <div id="container" ref="el"></div>
+  <div class="btn" @click="playorstopLottieAnime">点击切换Lottie动画播放状态</div>
 </template>
 
 
@@ -25,5 +37,12 @@ onMounted(() => {
   width: 200px;
   height: 200px;
   background-color: #06aeec;
+}
+.btn {
+  padding: 10px;
+  border-radius: 15px;
+  border: 1px solid black;
+  cursor: pointer;
+  margin: 10px auto 0;
 }
 </style>
