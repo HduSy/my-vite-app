3fb213874b40d263eaaa370bd50be59da0f0d1ee
HduSy <hdusy14032122@gmail.com>
2024-01-08 14:55:31 +0800

    feat: requestAnimationFrame

---
+++ src/components/RequestAnimationFrame.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  const box = document.querySelector(".box") as HTMLDivElement;
+  let anime = null;
+  let positionX = 0;
+  const animate = () => {
+    if (anime) cancelAnimationFrame(anime);
+    positionX -= 1;
+    if (positionX < -(5 * 100 + 5 * 20)) positionX = 0;
+    box.style.transform = `translateX(${positionX}px)`;
+    anime = requestAnimationFrame(animate);
+  };
+  animate();
+})
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="box">
+      <div class="child" style="background-color: red">这</div>
+      <div class="child" style="background-color: gold">里</div>
+      <div class="child" style="background-color: yellowgreen">是</div>
+      <div class="child" style="background-color: purple">三</div>
+      <div class="child" style="background-color: brown">土</div>
+      <div class="child" style="background-color: red">这</div>
+      <div class="child" style="background-color: gold">里</div>
+      <div class="child" style="background-color: yellowgreen">是</div>
+      <div class="child" style="background-color: purple">三</div>
+      <div class="child" style="background-color: brown">土</div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.wrapper {
+  width: calc(100px * 5 + 20px * 4);
+  margin: 60px auto 0;
+  border: 1px solid black;
+  overflow: hidden;
+}
+.box {
+  width: fit-content;
+  display: flex;
+}
+.box .child {
+  width: 100px;
+  height: 100px;
+  margin-left: 20px;
+  line-height: 100px;
+  text-align: center;
+  font-size: 33px;
+}
+.box .child:first-child {
+  margin-left: 0;
+}
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,23 @@
 
 import { createRouter, createWebHashHistory } from 'vue-router'
+const HelloWorld = () => import('../components/HelloWorld.vue')
 const Video  = () => import('../components/VideoItem.vue')
+const CSSModule = () => import('../components/CSSModule.vue')
 const PixiSpineAnime = () => import('../components/PixiSpineAnime.vue')
 const SpinePlayerAnime = () => import('../components/SpinePlayerAnime.vue')
-const HelloWorld = () => import('../components/HelloWorld.vue')
 const SVGAAnime = ()=>import('../components/SVGAAnime.vue')
-const CSSModule = () => import('../components/CSSModule.vue')
 const LottieAnime  = () => import('../components/LottieDemo.vue')
+const RequestAnimationFrameAnime = () => import('../components/RequestAnimationFrame.vue')
 
 const routes = [
   {path: '/', component: HelloWorld},
   {path:'/video', component: Video},
+  {path: '/css-module', component: CSSModule},
   {path: '/pixi-spine-demo', component: PixiSpineAnime},
   {path: '/spine-player-demo', component: SpinePlayerAnime},
-  {path: '/svgademo', component: SVGAAnime},
-  {path: '/css-module', component: CSSModule},
-  {path: '/lottie-demo', component: LottieAnime}
+  {path: '/svga-demo', component: SVGAAnime},
+  {path: '/lottie-demo', component: LottieAnime},
+  {path: '/requestAnimationFrameAnime-demo', component: RequestAnimationFrameAnime},
 ]
 
 const router = createRouter({
