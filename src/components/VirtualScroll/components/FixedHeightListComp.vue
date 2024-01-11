16a2bf07eff10d1eae70bbb233d0874200878850
HduSy <hdusy14032122@gmail.com>
2024-01-11 11:21:46 +0800

    feat: 固定高度虚拟列表

--- src/components/VirtualScroll/components/FixedHeightListComp.vue
+++ src/components/VirtualScroll/components/FixedHeightListComp.vue
@@ -9,100 +9,65 @@ const props = defineProps({
   itemHeight: Number,
 })
 
-const containerRef = ref<HTMLDivElement>()
+const { width = 200, height = 200, itemHeight = 50} = props
+
 const currentScollTop = ref(0)
+const containerRef = ref<HTMLDivElement>()
+let listData = ref<Array<any>>(Array.from({length: 10000}, (item, i) => i)); // 长列表
 const visiableData = ref<Array<number>>([])
+const visiableStyles = ref<Array<StyleValue>>([])
 
-const {width = 200, height = 200, itemHeight = 25} = props
-
-let listData = ref<Array<any>>([]);
-const vNode = ref<HTMLDivElement>()
-const ioInstance = ref<IntersectionObserver>()
-const loading = ref(false)
-
-/**
- * 监测列表底部虚节点曝光触底操作
- */
-const observeEl = () => {
-  ioInstance.value = new IntersectionObserver((entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
-    entries.forEach(async entry => {
-      if(entry.isIntersecting) {
-        console.log('曝光')
-        const newItems = await loadItems()
-        listData.value = [...listData.value, ...newItems]
-        console.log(listData.value)
-        // observer.unobserve(entry.target)
-      }
-    })
-  }, {
-
-  })
-  ioInstance.value.observe(vNode.value)
-}
-
-// 外部容器高度
+// 外部容器高度，内容区存在滚动，监听滚动的目标元素
 const containerStyle: StyleValue = {
   position: 'relative',
   width: `${width}px`,
   height: `${height}px`,
   overflow: 'auto',
 }
-
-// listData所有元素撑起盒子的实际高度
+// 所有元素撑起盒子的实际高度，超出container，产生滚动
 const contentStyle = computed(() => {
   return {
     width: '100%',
     height: `${itemHeight * listData.value.length}px`,
   }
 })
-
-const itemStyle = (index: number):StyleValue => ({
-  height: `${itemHeight}px`,
-  backgroundColor: index % 2 === 0 ? `#b478ed`:`#b1ed78`
-})
-
-const styles = ref<Array<StyleValue>>()
 // 更新可视区items
 const updateVisiableItems = () => {
-  const visiableIndex = Math.floor(currentScollTop.value/ itemHeight) // 可视区开始索引
-  const startIndex = Math.max(0, visiableIndex - 5) // 上缓冲区
-  const visiableSize = Math.ceil(height/itemHeight) // 可视区item数
-  const endIndex = Math.min(listData.value.length, startIndex + visiableSize + 10) // 下缓冲区
+  const visiableIndex = Math.floor(currentScollTop.value/ itemHeight)
+  const startIndex = Math.max(0, visiableIndex - 5)
+  const visiableSize = Math.ceil(height/itemHeight)
+  const endIndex = Math.min(listData.value.length, startIndex + visiableSize + 7)
   visiableData.value = listData.value.slice(startIndex, endIndex + 1)
+  console.log(`可视区开始索引 ${visiableIndex}`)
+  console.log(`上缓冲区 ${startIndex}`)
+  console.log(`可视区item数 ${visiableSize}`)
+  console.log(`下缓冲区 ${endIndex}`)
+  console.log(`visiableData `, visiableData.value)
   // 动态计算item style.top
-  const arr = [] as any
+  let arr = [] as any
   for(let i = startIndex; i <= endIndex; i++) {
     arr.push({
+      position: 'absolute',
       height: `${itemHeight}px`,
       top: `${itemHeight * i}px`,
       backgroundColor: i % 2 === 0 ? `#b478ed`:`#b1ed78`
     })
   }
-  styles.value = arr
+  visiableStyles.value = arr
 }
-// scroll事件节流，频繁触发，回调函数每100s内只执行一次
+// scroll事件节流，频繁触发，回调函数每100ms内只执行一次
 const containerScrollTop = throttle(() => {
-  currentScollTop.value = containerRef.value?.scrollTop || containerRef.value?.scrollTop || 0
+  const scrollTarget = containerRef.value
+  currentScollTop.value = scrollTarget?.scrollTop || scrollTarget?.scrollTop || 0
   updateVisiableItems()
 }, 100)
-// 模拟异步请求
-const loadItems = (): Promise<Array<any>> => {
-  loading.value = true
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log('懒加载新数据')
-      loading.value = false
-      resolve(new Array(10).fill(true))
-    }, Math.random() * 1000)
-  })
-}
 
 onMounted(async () => {
-  listData.value.push(...await loadItems())
-  observeEl()
+  updateVisiableItems()
+  containerRef.value?.addEventListener('scroll', containerScrollTop, false)
 })
 onUnmounted(() => {
-  vNode.value && ioInstance && ioInstance.unobserve(vNode.value)
+  containerRef.value?.removeEventListener('scroll', containerScrollTop, false)
 })
 </script>
 
@@ -110,13 +75,9 @@ onUnmounted(() => {
   <h3>固定高度虚拟列表</h3>
   <div class="container" ref="containerRef" :style="containerStyle">
     <div class="list" :style="contentStyle">
-      <div class="item" v-for="(item, index) in listData" :style="itemStyle(index)">
-        {{ index }}
+      <div class="item" v-for="(item, index) in visiableData" :style="visiableStyles[index]">
+        {{ item }}
       </div>
-      <div class="v-node" ref="vNode"></div>
-      <Transition name="fade">
-        <div v-if="loading" class="loading">loading...</div>
-      </Transition>
     </div>
   </div>
 </template>
