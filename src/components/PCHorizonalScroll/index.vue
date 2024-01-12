e75f7d8563c9ef13d1241b0c8f10ec8516c9b951
HduSy <hdusy14032122@gmail.com>
2024-01-12 11:28:23 +0800

    feat: 拖拽滚动

--- src/components/PCHorizonalScroll/index.vue
+++ src/components/PCHorizonalScroll/index.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
-import { onBeforeUnmount, onMounted, ref } from 'vue';
+import { computed, onBeforeUnmount, onMounted, ref } from 'vue';
+
+interface IProps {
+  containerWidth?: number
+}
+const props = withDefaults(defineProps<IProps>(), {
+  containerWidth: 700
+})
+
+const { containerWidth = 700 } = props
 /**
  * offsetX 鼠标指针相对于目标节点的内填充边的 X 轴坐标
  * clientX 鼠标指针相对于局部 DOM 元素的 X 轴坐标
@@ -8,16 +17,17 @@ const list = ref<Array<number>>(Array.from({length:10}, (item, i) => i))
 const containerRef = ref<HTMLDivElement>()
 const listRef = ref<HTMLDivElement>()
 
-var startX = 0
-var translateX = 0 // 横向滑动的距离
-var startTranslate // 每次滑动的起始值
-var isSlide = false // 是否滑动
-var maxWidth = ref(0) //最大滑动长度
+var isSlide = false // 滑动前提条件是，指定[元素上][鼠标按下][并移动]
+var startX = 0 // 开始滑动坐标，计算每次鼠标横向移动距离
+const translateX = ref(0) // 当前横向滑动的距离
+const startTranslate = ref(0) // 每次滑动的起始值，在此基础上更新 translateX
+var maxScrollWidth = ref(0) // 最大滑动长度
 
-function handleMouseDown(e) {
-  isSlide = true //开始滑动
-  startX = e.clientX // 开始滑动坐标
-  startTranslate = translateX
+// 鼠标按下
+function handleMouseDown(e:MouseEvent) {
+  isSlide = true // 开始滑动
+  startX = e.clientX
+  startTranslate.value = translateX.value // 更新滑动起始值
 }
 /**
  * translateX !important
@@ -25,15 +35,23 @@ function handleMouseDown(e) {
 function handleMouseMove(e) {
   if(isSlide) {
     const currentX = e.clientX // 当前滑动x坐标
-    if (startTranslate == 0 && currentX > startX) return
-    translateX = currentX - startX + startTranslate  // 横向滑动的终点坐标值
-    if(translateX > 0) { //假如超过零点，回到原点
-  	  translateX = 0
-    } else if (translateX <= maxWidth.value) {  //超过最大坐标，回到末点
-      translateX = maxWidth.value
+    if (startTranslate.value == 0 && currentX > startX) return // 初始态向右滑不动
+    translateX.value = currentX - startX + startTranslate.value  // 横向滑动的终点坐标值
+    // 对translateX进行范围限制
+    if(translateX.value > 0) { // 假如超过零点，回到原点
+  	  translateX.value = 0
+    } else if (translateX.value <= maxScrollWidth.value) {  //超过最大坐标，回到末点
+      translateX.value = maxScrollWidth.value
     }
-    listRef.value.style.transform = "translate(" + translateX + "px,0px) translateZ(0px)";
   }
+}
+const listStyle = computed(() => {
+  return {
+    transform: "translate(" + translateX.value + "px, 0) translateZ(0)"
+  }
+})
+const containerStyle = {
+  width: `${containerWidth}px`
 }
 function handleMouseLeave() {
   isSlide = false
@@ -44,7 +62,8 @@ function handleMouseUp() {
             
 onMounted(() => {
   const containerDiv = containerRef.value
-  maxWidth.value = -((list.value.length-1) * 110 + 100 - 700)
+  let listLen = (list.value.length - 1) * 110 + 100
+  maxScrollWidth.value = -(listLen - containerWidth)
   if(containerDiv) {
     containerDiv.addEventListener("mousedown", handleMouseDown, false)
     containerDiv.addEventListener("mousemove", handleMouseMove, false)
@@ -64,8 +83,8 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <div class="container" ref="containerRef">
-    <div class="list" ref="listRef">
+  <div class="container" ref="containerRef" :style="containerStyle">
+    <div class="list" ref="listRef" :style="listStyle">
       <div class="list-item" v-for="(item, index) in list">{{ index }}</div>
     </div>
   </div>
@@ -73,9 +92,9 @@ onBeforeUnmount(() => {
 
 <style scoped lang="scss">
 .container {
-  width: 700px;
   height: 200px;
-  overflow-x: scroll;
+  overflow-x: hidden;
+  /* overflow-x: auto; */
   border: 1px solid #ccc;
   position: relative;
   padding: 0 10px;
