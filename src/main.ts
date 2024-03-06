a9f4be8f5b50342297a8cbd8eb4e66220f7f57d5
HduSy <hdusy14032122@gmail.com>
2024-03-06 19:31:06 +0800

    feat: 测验运行时渲染组件

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,13 @@ const app = createApp(App)
 app.use(router)
 app.use(createPinia())
 app.mount('#app')
+
+// import NineLottery from './views/9Lottery/index.vue'
+
+// window['NineLottery'] = NineLottery
+
+// console.log('import 引入的 Component NineLottery.vue 到底是啥', window['NineLottery'])
+
+// setTimeout(() => {
+//   createApp(NineLottery).mount('#app')
+// }, 2000)
